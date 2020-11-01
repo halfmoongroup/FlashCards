f9b32aeb3cec852992d405962d9c20e6e3420de2
Tony Giaccone <tony@giaccone.org>
2020-11-01 00:22:04 -0400

    Update the datamodel, and add docs

---
+++ src/main/java/com/hmg/app/flash/model/AppUser.java
@@ -0,0 +1,9 @@
+package com.hmg.app.flash.model;
+
+import com.hmg.app.flash.model.auto._AppUser;
+
+public class AppUser extends _AppUser {
+
+    private static final long serialVersionUID = 1L; 
+
+}
