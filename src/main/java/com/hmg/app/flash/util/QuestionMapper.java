83eda38c3fef2f69995dea72ad0e60bfe1f91735
Tony Giaccone <tony@giaccone.org>
2020-11-02 10:01:39 -0500

    Unsaved change

--- src/main/java/com/hmg/app/flash/util/QuestionMapper.java
+++ src/main/java/com/hmg/app/flash/util/QuestionMapper.java
@@ -1,8 +1,13 @@
 package com.hmg.app.flash.util;
 
-public interface QuestionMapper {
-    QuestionMapper INSTANCE = Mappers.getMapper( Question.class ); 3
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-    
+import com.hmg.app.flash.api.request.QuestionDto;
+import com.hmg.app.flash.model.Question;
+
+@Mapper
+public interface QuestionMapper {
+	
     QuestionDto mapQuestion(Question aQuestion);
 }
