a4cda88e74b2137f6b3b09e47ae7b645eaee13f5
Tony Giaccone <tony@giaccone.org>
2020-11-08 23:33:44 -0500

    Remove old objects

--- src/main/java/com/hmg/app/flash/model/AppUser.java
+++
@@ -1,9 +0,0 @@
-package com.hmg.app.flash.model;
-
-import com.hmg.app.flash.model.auto._AppUser;
-
-public class AppUser extends _AppUser {
-
-    private static final long serialVersionUID = 1L; 
-
-}
--- src/main/java/com/hmg/app/flash/model/Test.java
+++
@@ -1,9 +0,0 @@
-package com.hmg.app.flash.model;
-
-import com.hmg.app.flash.model.auto._Test;
-
-public class Test extends _Test {
-
-    private static final long serialVersionUID = 1L; 
-
-}
